bed7b55f0f839f04d2b037d33daf36571fe19f73
puppyukii <babibabitha53@gmail.com>
2018-09-16 20:05:27 +0530

    demo

---
+++ models/user.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+// user Schema
+var UserSchema = mongoose.Schema({
+username:{
+      type:String,
+      index:true
+},
+   password:{
+      type:String
+   },
+   email:{
+     type:String
+   },
+   name:{
+     type:String
+   },
+});
+
+var User =module.exports = mongoose.model('user',UserSchema);
+
+module.exports.creatUser =function(newUser,callback){
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(newUser.password, salt, function(err, hash) {
+        newUser.password = hash;
+        newUser.save(callback);
+    });
+});
+}
+
+module.exports.getUserByUsername = function(newUser,callback){
+  var query = {username:username};
+  User.findOne(query,callback);
+}
+
+module.exports.getUserById = function(newUser,callback){
+
+  User.findById(id,callback);
+}
+
+module.exports.comparePassword = function(candidatePassword,hash,callback){
+  bcrypt.compare(candidatePassword, hash, function(err,isMatch) {
+   if(err)throw err;
+   callback(null,isMatch);
+});
+}
